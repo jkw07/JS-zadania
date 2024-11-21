b0cb6ea2f640dde488ba5c95c96d28534ec8522f
Justyna W <jkwodecka@gmail.com>
2024-11-21 18:17:14 +0100

    add zad7

---
+++ 7-rozne/script1.js
@@ -0,0 +1,22 @@
+/*Zad 1 
+  stwórz fukcję calculate (a,b,operator), która przyjmuje 3 argumenty: a - pierwsza liczba, b- druga liczba, operator: +,-,*,/
+  i na podstawie operatora wykonuje odpowiednią operację na liczbach a i b oraz zwraca wynik operacji*/
+
+function calculate(a,b,operator) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('Both inputs must be numbers');
+  }
+  switch (operator) {
+    case '-': return a-b
+    case '+': return a+b
+    case '*': return a*b
+    case '/': if (b=== 0) {
+      throw new Error("Division by zero is not allowed")
+    } return a/b;
+    default: throw new Error("Invalid operator");
+  }  
+}
+let a = 2;
+let b = 5;
+let operator = '+';
+console.log(calculate(a,b,operator));
---
+++ 7-rozne/script2.js
@@ -0,0 +1,39 @@
+/*Symulator kasy sklepowej
+Stwórz funkcję która symuluje prostą kasę sklepową. Twoim zadaniem będzie przećwiczenie operatorów matematycznych i logicznych
+Napisz funckję calculateTotal(items,doscount), która:
+- przyjmuje dwa argumenty: items - tablica cen produktów; discount - liczba określająca procentowy rabat, np. 10 oznacza 10% rabatu
+- oblicza łaczną sumę produktów
+- uwzględnia rabat jesli jest wiekszy od zera
+- zwraca ostateczną kwotę po rabacie
+- dodaj obsluge bledów: jesli items nie jest tablicą liczb lub discount nie jest liczbą, funkcja powinna wyrzucic błąd
+jesli rabat jest mniejszy niz 0 lub większy niz 100, rowniez rzuc błąd*/
+
+
+
+function calculateTotal(items,discount) {
+  if (!Array.isArray(items)) {
+    throw new Error('Items must be an array of numbers');
+  }
+  if (typeof discount !== 'number') {
+  throw new Error('Both inputs must be numbers');
+  }
+  if (discount < 0 || discount > 100) {
+    throw new Error('Change discount number');
+  }
+  if (items.length === 0) {
+    return 0;
+  }
+  let sum = 0;
+  for (const item of items) { 
+      sum += item;
+    }
+  const afterDiscount = (sum - (sum * discount / 100)).toFixed(2);
+  if (afterDiscount > 100) {
+   const bigDiscount = (afterDiscount - (afterDiscount * 10 / 100)).toFixed(2);
+   return bigDiscount;  
+  } else
+ return afterDiscount;
+}
+
+
+console.log(`Total payment is ${calculateTotal([100,20,20],50)}`)
