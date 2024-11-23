30608c344b69f714a99714ed79cc4a1189522248
Justyna W <jkwodecka@gmail.com>
2024-11-23 16:23:51 +0100

    Add sortedWords

---
+++ 7-rozne/sortedWords.js
@@ -0,0 +1,28 @@
+/*## Zadanie 9
+--------------------------
+Mamy przykładowy tekst:
+
+```
+const str = "Ania|Marcin|Bartek|Piotr|Kuba|Beata|Agnieszka";
+```
+
+Napisz funkcję, która przyjmie 2 atrybuty:
+- tekst
+- znak rozdziału (np. `|`)
+
+Skorzystaj z odpowiedniej metody, tak aby rozdzielić przekazany do funkcji tekst na części za pomocą przekazanego znaku rozdziału. W wyniku rozdzielenia powinieneś dostać tablicę. Funkcja niech posegreguje tą tablicę alfabetycznie. Następnie funkcja niech połączy tą tablicę w nowy tekst wstawiając między imiona znak wcześniejszego rozdziału. Skorzystaj tutaj z innej odpowiedniej metody js.
+
+```
+input: "Ania|Marcin|Bartek"
+wynik:
+"Ania|Bartek|Marcin"
+```*/
+
+const str = "Ania|Marcin|Bartek|Piotr|Kuba|Beata|Agnieszka";
+
+function sortedWords(text,separator) {
+  return text.split(separator).sort().join(separator);
+}
+
+console.log(str)
+console.log(sortedWords(str,"|"))
