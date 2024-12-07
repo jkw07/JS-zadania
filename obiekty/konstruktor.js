dd778476f8d7cc0c550a2006c5401bb84b15bca4
Justyna W <jkwodecka@gmail.com>
2024-12-07 19:20:56 +0100

    add musicPlayer

--- obiekty/konstruktor.js
+++ obiekty/konstruktor.js
@@ -1,5 +1,4 @@
 /* Stwórz konstruktor/klasę User. Każdy obiekt budowany na jego bazie powinien mieć właściwości:
-
 nick - string
 name - string
 surname - string
@@ -10,7 +9,6 @@ active - boolean - domyślnie ustawione na true - czy dany użytkownik jest akty
 Właściwości nick, name, surname, email, role powinny być podawane podczas tworzenia pojedynczego obiektu.
 
 Dodaj także metody:
-
 logIn(), która wstawi do tablicy loginDates nową datę logowania. Aktualną datę możesz pobrać za pomocą kodu new Intl.DateTimeFormat('pl-PL', { dateStyle: 'full', timeStyle: 'long' }).format(new Date())
 showLoginDates(), która w pętli pokaże wszystkie daty logowań
 showDetails(), która robiąc pętlę po kluczach wypisze w konsoli wszystkie informacje o danym użytkowniku
