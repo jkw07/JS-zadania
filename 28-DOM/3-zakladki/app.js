0e2517d873758e2be10364a719e79f62ec77c196
Justyna W <jkwodecka@gmail.com>
2024-12-28 21:14:38 +0100

    add 28-3

---
+++ 28-DOM/3-zakladki/app.js
@@ -0,0 +1,18 @@
+const tabElements = document.querySelectorAll(".tab-el");
+const links = document.querySelectorAll(".tab-el a");
+const divs = document.querySelectorAll(".tab-content");
+
+tabElements.forEach((tabEl) => {
+  const link = tabEl.querySelector("a");
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+    document.querySelector(".tab-el-active")?.classList.remove("tab-el-active");
+    tabEl.classList.add("tab-el-active");
+    divs.forEach((div) => div.classList.remove("tab-content-active"));
+    const targetId = link.getAttribute("href").substring(1);
+    const targetDiv = document.getElementById(targetId);
+    if (targetDiv) {
+      targetDiv.classList.add("tab-content-active");
+    }
+  });
+});
